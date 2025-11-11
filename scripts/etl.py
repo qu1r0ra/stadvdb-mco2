6190836fbfa86b940a6f56ba3afbf498b03f025f
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-11-12 03:22:24 +0800

    feat: add rider enum validation

--- scripts/etl.py
+++ scripts/etl.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 from scripts.configs import NODE_SPLITS_DIR
-from scripts.db.source import get_source_engine
+from scripts.db.source import CourierName, RiderVehicleType, get_source_engine
 
 
 def extract_riders_data() -> pd.DataFrame:
@@ -23,6 +23,22 @@ def extract_riders_data() -> pd.DataFrame:
     """
     riders_df = pd.read_sql(query, engine)
     return riders_df
+
+
+def validate_riders_enums(riders_df: pd.DataFrame):
+    invalid_couriers = riders_df.loc[
+        ~riders_df["courierName"].isin([e.value for e in CourierName])
+    ]
+    invalid_vehicles = riders_df.loc[
+        ~riders_df["vehicleType"].isin([e.value for e in RiderVehicleType])
+    ]
+
+    if not invalid_couriers.empty or not invalid_vehicles.empty:
+        raise ValueError(
+            f"Invalid values detected: \n"
+            f"Couriers: {invalid_couriers['courierName'].unique()}\n"
+            f"Vehicles: {invalid_vehicles['vehicleType'].unique()}"
+        )
 
 
 def transform_riders_data(riders_df: pd.DataFrame) -> pd.DataFrame:
@@ -45,16 +61,17 @@ def transform_riders_data(riders_df: pd.DataFrame) -> pd.DataFrame:
     new_df["createdAt"] = pd.to_datetime(new_df["createdAt"], errors="coerce")
     new_df["updatedAt"] = pd.to_datetime(new_df["updatedAt"], errors="coerce")
 
+    validate_riders_enums(new_df)
     return new_df
 
 
 def save_riders_data(riders_df: pd.DataFrame):
     os.makedirs(NODE_SPLITS_DIR, exist_ok=True)
     riders_df.to_csv(NODE_SPLITS_DIR / "node1_full.csv", index=False)
-    riders_df[riders_df["courierName"] == "JNT"].to_csv(
+    riders_df[riders_df["courierName"] == CourierName.JNT].to_csv(
         NODE_SPLITS_DIR / "node2_fragment.csv", index=False
     )
-    riders_df[riders_df["courierName"] != "JNT"].to_csv(
+    riders_df[riders_df["courierName"] != CourierName.JNT].to_csv(
         NODE_SPLITS_DIR / "node3_fragment.csv", index=False
     )
 
