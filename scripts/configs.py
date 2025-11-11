599368f78178f16120bac4f0dffbf08dbfb4976e
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-11-12 03:44:18 +0800

    feat: add paths

--- scripts/configs.py
+++ scripts/configs.py
@@ -7,5 +7,10 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 DATA_DIR = PROJECT_ROOT / "data"
+SCRIPTS_DIR = PROJECT_ROOT / "scripts"
 
 NODE_SPLITS_DIR = DATA_DIR / "node_splits"
+
+DB_SCRIPTS_DIR = SCRIPTS_DIR / "db"
+
+NODE_SCHEMA_FILE = DB_SCRIPTS_DIR / "node.py"
