6fb2377bea2ebf70be5f3d103025be97b78d026d
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-11-15 07:45:58 +0800

    Create applyLog.ts

---
+++ src/utils/applyLog.ts
@@ -0,0 +1,21 @@
+import { Pool } from "mysql2/promise";
+
+export async function applyLogToNode(targetPool: Pool, log: any) {
+  const action = log.action;
+  const newV = log.new_value ? JSON.parse(log.new_value) : null;
+  const oldV = log.old_value ? JSON.parse(log.old_value) : null;
+
+  switch (action) {
+    case "INSERT":
+      await targetPool.query(`REPLACE INTO Riders SET ?`, newV);
+      break;
+
+    case "UPDATE":
+      await targetPool.query(`UPDATE Riders SET ? WHERE id = ?`, [newV, newV.id]);
+      break;
+
+    case "DELETE":
+      await targetPool.query(`DELETE FROM Riders WHERE id = ?`, [oldV.id]);
+      break;
+  }
+}
